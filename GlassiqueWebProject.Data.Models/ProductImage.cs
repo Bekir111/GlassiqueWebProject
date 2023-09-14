3728bb9cb3e066a94430d16f90dfec6bcc0c9a81
Win10 <Win10@DESKTOP-OGDQVM1>
2023-09-14 11:42:35 +0300

    Add project files.

---
+++ GlassiqueWebProject.Data.Models/ProductImage.cs
@@ -0,0 +1,11 @@
+﻿namespace GlassiqueWebProject.Data.Models
+{
+    public class ProductImage
+    {
+        public int ProductImageId { get; set; }
+        public int ProductId { get; set; } // Foreign key to the Product entity
+        public byte[] ImageData { get; set; } = null!;
+        public string ImageMimeType { get; set; } = null!;
+    }
+
+}
---
+++ GlassiqueWebProject.Data/GlassiqueDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlassiqueWebProject.Data
+{
+    public class GlassiqueDbContext : IdentityDbContext
+    {
+        public GlassiqueDbContext(DbContextOptions<GlassiqueDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
